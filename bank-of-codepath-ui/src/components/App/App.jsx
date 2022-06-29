7b87bfff345e84997691187939d93a81aac86efb
Courtney Fenderson <cfenderson@cfender-ltmv2wd.internal.salesforce.com>
2022-06-29 11:07:01 -0700

    step1

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -1,13 +1,37 @@
-import * as React from "react"
-import Navbar from "../Navbar/Navbar"
-import Home from "../Home/Home"
-import "./App.css"
+import * as React from "react";
+import Navbar from "../Navbar/Navbar";
+import Home from "../Home/Home";
+import TransactionDetail from "../TransactionDetail/TransactionDetail";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import "./App.css";
 
 export default function App() {
+  const [isLoading, setisLoading] = React.useState(false);
+  const [transactions, setTransactions] = React.useState([]);
+  const [transfers, setTransfers] = React.useState([]);
+  const [errors, setErrors] = React.useState(null);
+  const [FilterInputValue, setFilterInputValue] = React.useState("");
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+
+        <main>
+          <Routes>
+            <Route path="/" component={<Home />} />
+            <Route
+              path="/transactions/:transactionId"
+              component={<TransactionDetail />}
+            />
+          </Routes>
+          {/* <Home /> */}
+        </main>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
